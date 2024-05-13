7668d492d63b9079185a140562795007fa0b6a48
Sarvesh <112638058+sarvesh4522@users.noreply.github.com>
2024-05-13 15:20:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, send_from_directory
 import pandas as pd
 
 app = Flask(__name__)
@@ -15,7 +15,7 @@ learner_data = df_learner[['index', 'resource_name', 'x', 'y', 'description']].t
 
 @app.route('/')
 def index():
-    return render_template('index3.html')
+    return send_from_directory('.','index3.html')
 
 @app.route('/data')
 def get_data():
