495b5a495e0c9df0f991c30e8272b418774972a8
Sarvesh <112638058+sarvesh4522@users.noreply.github.com>
2024-05-13 15:18:13 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, jsonify
+import pandas as pd
+
+app = Flask(__name__)
+
+# Read data from Excel file
+excel_file = 'DM_Resource_Plot.xlsx'
+df = pd.read_excel(excel_file)
+excel_file = 'DM_learner_plot.xlsx'
+df_learner = pd.read_excel(excel_file)
+
+# Assuming your Excel file has columns 'x', 'y', and 'video_url'
+scatterplot_data = df[['index','name', 'x', 'y', 'video_url']].to_dict(orient='records')
+learner_data = df_learner[['index', 'resource_name', 'x', 'y', 'description']].to_dict(orient='records')
+
+@app.route('/')
+def index():
+    return render_template('index3.html')
+
+@app.route('/data')
+def get_data():
+    return jsonify(scatterplot_data)
+
+@app.route('/new_positions')
+def get_new_data():
+    return jsonify(learner_data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
